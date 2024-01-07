6055dbaeff5b9874474ac6599bcc84c9583930f6
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-01-06 23:29:19 -0500

    added five day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,23 @@ let date = document.querySelector("#date")
 let cityName = document.querySelector("#city-name")
 let weatherIcon = document.querySelector("#icon")
 let temp = document.querySelector("#temp")
+let temp1 = document.querySelector("#temp1")
+let temp2 = document.querySelector("#temp2")
+let temp3 = document.querySelector("#temp3")
+let temp4 = document.querySelector("#temp4")
+let temp5 = document.querySelector("#temp5")
 let humidity = document.querySelector("#humidity")
+let humidity1 = document.querySelector("#humidity1")
+let humidity2 = document.querySelector("#humidity2")
+let humidity3 = document.querySelector("#humidity3")
+let humidity4 = document.querySelector("#humidity4")
+let humidity5 = document.querySelector("#humidity5")
 let windSpeed = document.querySelector("#wind-speed")
+let windSpeed1 = document.querySelector("#wind-speed1")
+let windSpeed2 = document.querySelector("#wind-speed2")
+let windSpeed3 = document.querySelector("#wind-speed3")
+let windSpeed4 = document.querySelector("#wind-speed4")
+let windSpeed5 = document.querySelector("#wind-speed5")
 let submitButton = document.querySelector("#submit-button")
 
 
@@ -19,7 +34,7 @@ function currentWeather() {
     })
     .then(function (data) {
       console.log(data)
-
+      //define variables for latitude and longitude to be used for fetching the weather values
       const geoCodeLat = data[0].lat
       const geoCodeLon = data[0].lon
       const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoCodeLat}&lon=${geoCodeLon}&appid=${apiKey}`;
@@ -27,11 +42,9 @@ function currentWeather() {
       fetch(weatherUrl)
         .then(function (response) {
           return response.json();
-
         })
         .then(function (weatherData) {
           console.log(weatherData);
-
 
           const weatherHeading = document.querySelector("#weather-heading")
           weatherHeading.innerHTML = "Current Weather"
@@ -39,28 +52,43 @@ function currentWeather() {
           let iconCode = weatherData.list[0].weather[0].icon
           weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
 
+          //current weather: display city name, temp(convert to fahrenheit), humidity, and wind speed for current forecast
           cityName.innerText = data[0].name
-          temp.innerText = weatherData.list[1].main.temp
-          //change to F
-          humidity.innerText = weatherData.list[1].main.humidity
+
+          humidity.innerText = weatherData.list[1].main.humidity + "%"
+          humidity1.innerText = weatherData.list[1].main.humidity + "%"
+          humidity2.innerText = weatherData.list[8].main.humidity + "%"
+          humidity3.innerText = weatherData.list[16].main.humidity + "%"
+          humidity4.innerText = weatherData.list[24].main.humidity + "%"
+          humidity5.innerText = weatherData.list[32].main.humidity + "%"
+
           windSpeed.innerText = weatherData.list[1].wind.speed + "mph"
+          windSpeed1.innerText = weatherData.list[1].wind.speed + "mph"
+          windSpeed2.innerText = weatherData.list[8].wind.speed + "mph"
+          windSpeed3.innerText = weatherData.list[16].wind.speed + "mph"
+          windSpeed4.innerText = weatherData.list[24].wind.speed + "mph"
+          windSpeed5.innerText = weatherData.list[32].wind.speed + "mph"
+
+          let tempfh = (((weatherData.list[1].main.temp) - 273.15) * 1.8) + 32;
+          let temp1fh = (((weatherData.list[1].main.temp) - 273.15) * 1.8) + 32;
+          let temp2fh = (((weatherData.list[8].main.temp) - 273.15) * 1.8) + 32;
+          let temp3fh = (((weatherData.list[16].main.temp) - 273.15) * 1.8) + 32;
+          let temp4fh = (((weatherData.list[24].main.temp) - 273.15) * 1.8) + 32;
+          let temp5fh = (((weatherData.list[32].main.temp) - 273.15) * 1.8) + 32;
+
+          temp.innerText = tempfh.toFixed(0) + "°F";
+          temp1.innerText = temp1fh.toFixed(0) + "°F";
+          temp2.innerText = temp2fh.toFixed(0) + "°F";
+          temp3.innerText = temp3fh.toFixed(0) + "°F";
+          temp4.innerText = temp4fh.toFixed(0) + "°F";
+          temp5.innerText = temp5fh.toFixed(0) + "°F";
+ 
         })
     })
   
 }
 
 submitButton.addEventListener("click", currentWeather);
-
-// function fiveDayForecast() {
-//   currentWeather()
-//   console.log(data.city.coord.lat)
-
-
-
-// }
-
-
-
 
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
