abccd4dfeac7791b1bc407369dda5547ed75d0fe
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-01-10 14:43:49 -0500

    added 5-day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,30 +29,30 @@ function searchWeather() {
     })
     .then(function (data) {
       console.log(data)
-      
+
       //define variables for latitude and longitude to be used for fetching the weather values
       const geoCodeLat = data[0].lat
       const geoCodeLon = data[0].lon
 
       //now fetch the weather data using lon and lat
       const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoCodeLat}&lon=${geoCodeLon}&appid=${apiKey}`;
-      
+
       fetch(weatherUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (weatherData) {
           console.log(weatherData);
-        displayCurrentWeather(weatherData);  
+          displayCurrentWeather(weatherData);
         });
 
-//update UI elements
+      //update UI elements
       const weatherHeading = document.querySelector("#weather-heading")
       weatherHeading.innerHTML = "Current Weather"
 
       const todaysDate = document.querySelector("#todays-date")
       todaysDate.innerText = dayjs().format("MMM D, YYYY")
-      
+
     });
 
 }
@@ -68,17 +68,69 @@ function displayCurrentWeather(weatherData) {
   weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
   const currentTemperature = (((weatherData.list[0].main.temp) - 273.15) * 1.8) + 32;
-  currentTemp.innerHTML = `${currentTemperature.toFixed(0)} °F`;
-
-  const humidity = weatherData.list[0].main.humidity;
-  currentHumidity.innerHTML = `${humidity}%`;
+  currentTemp.innerHTML = `Temperature: ${currentTemperature.toFixed(0)} °F`;
 
   const wind = weatherData.list[0].wind.speed;
-  windSpeed.innerHTML = `${wind.toFixed(2)} m/s`;
+  windSpeed.innerHTML = `Wind Speed: ${wind.toFixed(2)} m/s`;
+
+  const humidity = weatherData.list[0].main.humidity;
+  currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+
+
+
+  displayForecast(weatherData);
 }
 
+// function to display 5-day forecast
 function displayForecast(weatherData) {
-  // Display 5-day forecast
+  //variable to link to HTML tag
+  const forecastContainer = document.querySelector("#forecast-container");
+
+  // Clears existing content in the forecast container
+  forecastContainer.innerHTML = "";
+
+  //Variable to identify which array items are to be pulled from
+  const forecastIndices = [8, 16, 24, 30, 38];
+  //Pulls data for each of the array items identified in the array above
+  forecastIndices.forEach(index => {
+    const dayData = weatherData.list[index];
+
+    // Creates HTML elements for each day
+    const dayContainer = document.createElement("div");
+    dayContainer.classList.add("forecast-day");
+
+    // Displays date for each day and appends to the <p> tag that was created
+    const dateElement = document.createElement("h5");
+    dateElement.innerText = dayjs(dayData.dt_txt).format("MMM D, YYYY");
+    dayContainer.appendChild(dateElement);
+
+    // Displays weather icons and appends to the <img> tag that was created
+    const iconElement = document.createElement("img");
+    const iconCode = dayData.weather[0].icon;
+    iconElement.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    //Adds an alt for accessibility
+    iconElement.alt = "Weather Icon";
+    dayContainer.appendChild(iconElement);
+
+    // Displays temperature and appends to the <p> tag that was created
+    const tempElement = document.createElement("p");
+    const tempElementFh = (((dayData.main.temp) - 273.15) * 1.8) + 32;
+    tempElement.innerText = `Temperature: ${tempElementFh.toFixed(0)} °F`;
+    dayContainer.appendChild(tempElement);
+
+    // Displaya wind speed and appends to the <p> tag that was created
+    const windElement = document.createElement("p");
+    windElement.innerText = `Wind Speed: ${dayData.wind.speed} m/s`;
+    dayContainer.appendChild(windElement);
+
+    // Displays humidity and appends to the <p> tag that was created
+    const humidityElement = document.createElement("p");
+    humidityElement.innerText = `Humidity: ${dayData.main.humidity}%`;
+    dayContainer.appendChild(humidityElement);
+
+    // Appends the day container to the forecast container
+    forecastContainer.appendChild(dayContainer);
+  });
 }
 
 function displaySearchHistory(history) {
